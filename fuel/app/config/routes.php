220d2694a17b41666e16167c99c4fad64eb4184b
mc-matsui <matsui@m-craft.com>
2016-08-05 18:10:25 +0900

    develop

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -6,8 +6,3 @@
  	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
  );
 
-return array(
-		'_root_'  => 'index',  // The default route
-		'_404_'   => '404',    // The main 404 route
-		//'bbs/(:num)'=> 'bbs/index/$1', //ページネーションのURL変更
-);
